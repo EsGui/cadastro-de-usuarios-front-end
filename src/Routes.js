866611ffacbc24a7136d6a542db519a5d621485d
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-03-06 20:16:40 -0300

    feat: Arquivo de rotas criado

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Switch } from 'react-router-dom'
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
