cc737ab0b95dfac01ff3a6651cab19efe17eb5c8
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-03-06 21:43:30 -0300

    feat: requisições adicionadas

---
+++ src/Services/RequestUsers.js
@@ -0,0 +1,46 @@
+const requestUsers = {
+    registerUsers: async ({ 
+        email,
+        password,
+        confirmPassword,
+    }) => {
+        const url = "https://cadastro-de-usuarios-back-hx3xrx9te-esgui.vercel.app/registration"
+        const request = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                email, 
+                password,
+                confirmPassword,
+            })
+        });
+        const response = await request.json()
+        return response 
+    },
+
+    deleteUsers: async ({ email }) => {
+        const url = "https://cadastro-de-usuarios-back-hx3xrx9te-esgui.vercel.app/delete-users"
+        const request = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                email,
+            })
+        });
+        const response = await request.json()
+        return response 
+    },
+
+    getUsers: async () => {
+        const url = "https://cadastro-de-usuarios-back-hx3xrx9te-esgui.vercel.app/users";
+        const request = await fetch(url);
+        const response = await request.json();
+        return response;
+    }
+}
+
+export default requestUsers;
