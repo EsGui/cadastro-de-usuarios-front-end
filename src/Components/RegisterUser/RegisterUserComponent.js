ce7c8ba3caf9c3d6a94c43f7b87ae0d6d0911a02
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-03-06 21:00:38 -0300

    feat: o Componente de login foi implementado

--- src/Components/RegisterUser/RegisterUserComponent.js
+++ src/Components/RegisterUser/RegisterUserComponent.js
@@ -1,11 +1,19 @@
-'./RegisterUserComponent.css'
+import './RegisterUserComponent.css';
 
 function RegisterUserComponent() {
-    return (
-        <div>
-            <h1>Iniciando projeto</h1>
+  return (
+    <div className="DivFatherRegisterUserComponent">
+        <div className="DivSonRegisterUserComponent">
+            <input type="email" placeholder="Digite seu email" />
+            <input type="password" placeholder="Crie uma senha" />
+            <input type="password" placeholder="Confirme sua senha" />
+            <button type="button" >Registrar usuário</button>
         </div>
-    )
+        <div className="DivSonRegisterUserComponent1">
+          <p>As contas criadas apareção aqui</p>
+        </div>
+    </div>
+  )
 }
 
 export default RegisterUserComponent;
