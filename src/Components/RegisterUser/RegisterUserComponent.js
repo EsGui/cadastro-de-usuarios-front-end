15f5c9560cac7f0ab48d71cdafe0ecd292f95c83
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2023-03-06 21:48:37 -0300

    feat: lógica para cadastrar e apagar usuários foi adicionado

--- src/Components/RegisterUser/RegisterUserComponent.js
+++ src/Components/RegisterUser/RegisterUserComponent.js
@@ -1,16 +1,41 @@
+import { useContext } from 'react';
+import myContext from '../../Context/myContext';
 import './RegisterUserComponent.css';
 
 function RegisterUserComponent() {
+  const {
+    functionRegisterUsersEmail,
+    functionRegisterUsersPassword,
+    functionRegisterUsersConfirmPassword,
+    functionButtonUsers,
+    functionDeleteUser,
+    listUsers
+  } = useContext(myContext);
+
+  console.log(listUsers)
+
   return (
     <div className="DivFatherRegisterUserComponent">
         <div className="DivSonRegisterUserComponent">
-            <input type="email" placeholder="Digite seu email" />
-            <input type="password" placeholder="Crie uma senha" />
-            <input type="password" placeholder="Confirme sua senha" />
-            <button type="button" >Registrar usuário</button>
+            <input type="email" placeholder="Digite seu email" onChange={ functionRegisterUsersEmail } />
+            <input type="password" placeholder="Crie uma senha" onChange={ functionRegisterUsersPassword } />
+            <input type="password" placeholder="Confirme sua senha" onChange={ functionRegisterUsersConfirmPassword } />
+            <button type="button" onClick={ functionButtonUsers }>Registrar usuário</button>
         </div>
         <div className="DivSonRegisterUserComponent1">
           <p>As contas criadas apareção aqui</p>
+            {
+              listUsers.map(({
+                email,
+                password,
+              }) => (
+                <div className="DivSpecificAccount">
+                    <p>{ email }</p>
+                    <p>{ password }</p>
+                    <button type="button" name={ email } onClick={ functionDeleteUser }>Deletar</button>
+                </div>
+              ))
+            }
         </div>
     </div>
   )
--- src/Context/Logic.js
+++ src/Context/Logic.js
@@ -1,9 +1,52 @@
-import myContext from "./myContext";
+import { useEffect, useState } from 'react';
+import requestUsers from '../Services/RequestUsers';
+import myContext from './myContext';
 
-function Logic({ children }) {
-    const obj = {}
+function Logic ({ children }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [listUsers, setListUsers] = useState([]);
 
-    return(
+    useEffect(() => {
+        const request = async () => {
+            requestUsers.getUsers().then((data) => {
+                setListUsers(data.message)
+            })
+        };
+        request()
+    }, [])
+
+    const functionRegisterUsersEmail = ({ target: { value } }) => setEmail(value);
+    const functionRegisterUsersPassword = ({ target: { value } }) => setPassword(value);
+    const functionRegisterUsersConfirmPassword = ({ target: { value } }) => setConfirmPassword(value);
+    const functionButtonUsers = async () => {
+        await requestUsers.registerUsers({
+            email,
+            password,
+            confirmPassword
+        })
+        document.location.reload()        
+    }
+
+    const functionDeleteUser = async ({ target: { name } }) => {
+        const email = name;
+        await requestUsers.deleteUsers({
+            email,
+        });
+        document.location.reload()
+    }
+
+    const obj = {
+        functionRegisterUsersEmail,
+        functionRegisterUsersPassword,
+        functionRegisterUsersConfirmPassword,
+        functionButtonUsers,
+        functionDeleteUser,
+        listUsers,
+    };
+
+    return (
         <myContext.Provider value={ obj }>
             { children }
         </myContext.Provider>
